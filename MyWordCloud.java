6891ee7a11ba7b41929884a07c9c950714f19d31
Aqueuse <67499074+Aqueuse@users.noreply.github.com>
2020-11-13 18:48:35 +0100

    Update MyWordCloud.java
    make it more generic and easy to use (just set the CSV path and the wanted image size)

--- MyWordCloud.java
+++ MyWordCloud.java
@@ -1,57 +1,147 @@
 package automationPoleEmploi;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
-public class MyWordCloud {
+import javax.imageio.ImageIO;
 
-	// prend un CSV avec les mots et leur fréquence
-	public MyWordCloud() throws IOException {
-		Random placement = new Random();
-		
-		String[][] words = readCSV.loadFile("motscles.csv");
+public class paintWordCloud {
+	static String[][] words;
+	static int CurrentImageWidth;
+	static int CurrentImageHeight;
+	
+	public static void main(String path, int width, int height) throws IOException {
+		CurrentImageWidth=width;
+		CurrentImageHeight=height;
+		Finalpaint(path, overlapManaged(initialPlacement()), width, height);
+	}
+
+	// convert data from CSV to array : add defaults coords
+	// random rotate, appropriate FontSize, and compute width
+	// and height for the overlap management
+	public static int[][] initialPlacement() throws IOException {
+		words = readCSV.loadFile("motscles.csv");
 		int[] textSize = {0,0};
 		int fontSize[] = FontSize(words);
 
-		int[][] coordWords = new int[4][words[0].length];
+		Random rotate = new Random();
+		rotate.setSeed(40);
+		int[][] coordWords = new int[6][words[0].length];
 		
-		// initiale feeding of the array of words :
-		// coordWords [x, y, width, height, fontSize]
+		// coordWords [x, y, width, height, fontSize, rotate]
 		for (int i=0; i<words[0].length; i++) {
-			coordWords[0][i] = 0;
-			coordWords[1][i] = 0;	
+			coordWords[0][i] = 0; // initial x
+			coordWords[1][i] = 0; // initial y
 
-			coordWords[4][i] = fontSize[i];
+			coordWords[4][i] = fontSize[i]; // fontSize
 
-			textSize = text(words[0][i], fontSize[i]);
-			coordWords[2][i] = textSize[0];
-			coordWords[3][i] = textSize[1];
+			textSize = textBox(words[0][i], fontSize[i]);
+			coordWords[2][i] = textSize[0]; // width
+			coordWords[3][i] = textSize[1]; // height
+			coordWords[5][i] = rotate.nextInt(2)*90; // rotation
 		}
-		
-		// place randomly one text and check if
-		// it's overlap an other text
-		// if so decal it and recheck
-		
+		return coordWords;
+	}
+	
+	public static int[][] overlapManaged(int[][] coordWords) {
+		int[][] coordWordsCorrected = coordWords;
+
+		Random placement = new Random();
+		placement.setSeed(40);
+
+		for (int c=0; c<coordWords[0].length; c++) {
+			coordWordsCorrected[0][c] = placement.nextInt(CurrentImageWidth);
+			coordWordsCorrected[1][c] = placement.nextInt(CurrentImageHeight);
+
+			for (int o=0; o<coordWords[0].length; o++) {
+				if (coordWordsCorrected[0][c] < coordWords[0][o]) {
+					coordWordsCorrected[0][c] = 0;
+					coordWordsCorrected[1][c] = 0;
+				}
+			}
+		}
+		return coordWordsCorrected;
 	}
 
 	// determine the fontSize based on the frequency
-	private int[] FontSize(String[][] ArrayWords) {
-		int[] sizeFont = null;
-		
+	private static int[] FontSize(String[][] ArrayWords) {
+		int[] sizeFont = new int[ArrayWords[0].length];
+		int currentFreq1;
+		int currentFreq2;
+
+		int maxFreq=0;
+		int minFreq=1;
+
 		// determine the max and min frequency
-		
+		for (int i=0; i<ArrayWords[0].length; i++) {
+			currentFreq1=Integer.valueOf(ArrayWords[1][i]);
+
+			for (int l=0; l<ArrayWords[0].length; l++) {
+				currentFreq2=Integer.valueOf(ArrayWords[1][l]);
+				if (currentFreq2>currentFreq1 && currentFreq2>maxFreq) {
+					maxFreq=currentFreq2;
+				}
+				if (currentFreq2<currentFreq1 && currentFreq2<minFreq) {
+					minFreq=currentFreq2;
+				}
+			}
+		}
+
 		// calculate the font size base of the
 		// min, max freq and the freq of the word
-		
+		int averageFont = (maxFreq+minFreq)/2;
+
+		for (int f=0; f<ArrayWords[0].length; f++) {
+			sizeFont[f]=averageFont*Integer.valueOf(ArrayWords[1][f])*10;
+		}
 		return sizeFont ;
 	}
 
-	// determine the size of the text zone of one word based on its
-	// letters and fontSize
-	private int[] text(String string, int FontSize) {
-		int[] textSize = {1,1};
+	// determine the size of the text zone of one word based
+	// on its chars and the fontSize
+	private static int[] textBox(String word, int fontSize) {
+		int[] Textsize= {0,0};
 
-		return textSize;
+		long charNb=word.chars().count();
+		Textsize[0]=(int) (charNb*fontSize); // width
+		Textsize[1]=(int) (fontSize); 		 // height
+
+		return Textsize;
+	}
+	
+	// the effective textWriting
+	private static void textWrite(String string, int FontSize, int x, int y, int rotate, Graphics g2) {
+		// todo : randomize color
+		Color BleuCiel = new Color(45, 142, 247);
+
+		AffineTransform affineTransform = new AffineTransform();
+		affineTransform.rotate(Math.toRadians(rotate), 0, 0);
+
+		Font font = new Font("TimesRoman", Font.PLAIN, FontSize);
+		Font rotatedFont = font.deriveFont(affineTransform);
+		g2.setColor(BleuCiel);
+		g2.setFont(rotatedFont);
+
+		g2.drawString(string, x, y);
 	}
 
+	static void Finalpaint(String path, int[][] coordWords, int ImageWidth, int ImageHeight) {		
+		BufferedImage image = new BufferedImage(ImageWidth, ImageHeight, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = image.createGraphics();
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		for (int w=0; w<coordWords[0].length; w++) {
+			textWrite(words[0][w], coordWords[4][w], coordWords[0][w], coordWords[1][w], coordWords[5][w], g2);
+		}
+		
+		try { ImageIO.write(image, "png", new File("test.png")); } catch (IOException e) {}
+	}
 }
